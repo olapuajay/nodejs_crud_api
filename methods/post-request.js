920eff8ac4502f3357f9a71eca64e9f71968a85b
Olapu Ajay <olapuajay@gmail.com>
2025-03-08 19:42:31 +0530

    implement POST request handler and body parser utility

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,13 @@
-module.exports = (req, res) => {};
+const crypto = require('crypto');
+const reqBodyParser = require('../util/body-parser');
+
+module.exports = async (req, res) => {
+  if(req.url === "/api/movies") {
+    try {
+      let body = await reqBodyParser(req);
+      console.log("Request Body: ", body);
+    } catch (error) {
+      
+    }
+  }
+};

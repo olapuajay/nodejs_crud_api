e13bb312b723ed4333872f68d9bca38f2a1b9d63
Olapu Ajay <olapuajay@gmail.com>
2025-03-11 10:36:45 +0530

    implement the post request

--- methods/post-request.js
+++ methods/post-request.js
@@ -5,9 +5,20 @@ module.exports = async (req, res) => {
   if(req.url === "/api/movies") {
     try {
       let body = await reqBodyParser(req);
+      body.id = crypto.randomUUID();
+      req.movies.push(body);
       console.log("Request Body: ", body);
-    } catch (error) {
-      
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end();
+    } catch (err) {
+      console.log(err);
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          title: "Validation Failed",
+          message: "Request body is not valid"
+        })
+      );
     }
   }
 };

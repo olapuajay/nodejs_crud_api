fbfdb9f3cdd27311a1a7bb73491a32dbe385e5f9
Olapu Ajay <olapuajay@gmail.com>
2025-03-12 18:58:52 +0530

    Implement the delete request

--- methods/delete-request.js
+++ methods/delete-request.js
@@ -1,1 +1,30 @@
-module.exports = (req, res) => {};
+const writeToFile = require("../util/write-to-file");
+
+module.exports = (req, res) => {
+  let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1);
+  let id = req.url.split("/")[3];
+  const regex = new RegExp(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/i);
+
+  if(!regex.test(id)) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({ title: "Validation failed", message: "UUID is not valid" })
+    );
+  } else if(baseUrl === "/api/movies/" && regex.test(id)) {
+    const index = req.movies.findIndex((movie) => {
+      return movie.id === id;
+    })
+    if(index === -1) {
+      res.statusCode = 404;
+      res.write(
+        JSON.stringify({ title: "Not Found", message: "Movie not found" })
+      )
+      res.end();
+    } else {
+      req.movies.splice(index, 1);
+      writeToFile(req.movies);
+      res.writeHead(204, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(req.movies));
+    }
+  }
+};
